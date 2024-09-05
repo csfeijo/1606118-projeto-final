f94f45d2b523a2f4c311235cd105541b277f261f
Cícero Feijó <cicero.feijo@gmail.com>
2024-09-05 19:58:38 -0300

    feat: exercicio ui do formulario de departamentos

---
+++ src/Pages/Departamentos/Forms/index.tsx
@@ -0,0 +1,64 @@
+import { Button } from "primereact/button"
+import { FloatLabel } from "primereact/floatlabel"
+import { InputText } from "primereact/inputtext"
+import { useNavigate } from "react-router-dom"
+
+const FormDepartamento = () => {
+
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <div className="col-span-12">
+
+        <div className="flex justify-between items-center my-6">
+          <h2 className="text-2xl font-bold">Cadastro de Departamento</h2>
+          <Button
+            icon="pi pi-chevron-left"
+            label="voltar"
+            severity="info"
+            rounded
+            onClick={() => {
+              navigate('/departamentos')
+            }}
+          />
+        </div>
+
+      </div>
+      {/*  SEGUNDA LINHA */}
+      <div className="col-span-6">
+        <FloatLabel>
+          <InputText
+            id="nome"
+            className="w-full"
+          />
+          <label htmlFor="nome">Nome</label>
+        </FloatLabel>
+      </div>
+
+      <div className="col-span-6">
+       <FloatLabel>
+          <InputText
+            id="sigla"
+          />
+          <label htmlFor="sigla">Sigla</label>
+        </FloatLabel>
+      </div>
+
+      {/* TERCEIRA LINHA */}
+      <div className="col-span-12">
+        <Button
+          label="salvar"
+          severity="success"
+          icon="pi pi-save"
+          className="w-full"
+        />
+      </div>
+      
+
+
+    </>
+  )
+}
+
+export default FormDepartamento
