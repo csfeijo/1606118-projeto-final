efb5b85b7af91059058c81568cb185ab8e2edbf4
Cícero Feijó <cicero.feijo@gmail.com>
2024-09-16 21:41:13 -0300

    feat: remove link nao usado do antigo menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Card } from 'primereact/card'
 import Menu from './Components/Menu'
 import AppRoutes from './Routes'
